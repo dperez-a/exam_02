2fbe9af2d9d6c9dd3fa3f2598a6e61b5394cd708
Daniel Perez Ayuso <dperez-a@c2r11s3.42madrid.com>
2024-10-30 16:03:12 +0100

    rev_print made

---
+++ levels/lvl1_practice/rev_print/rev_print.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+char    *rev_print(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+        i++;
+    while (i > 0)
+    {
+        i--;
+        write(1, &str[i], 1);
+    }
+    return (str);
+}
+
+char *rev_print(char *str);
+
+int main() {
+    char *str = "Hola";
+    rev_print(str);
+    write(1, "\n", 1);  // Salto de línea después de la cadena invertida
+    return 0;
+}
